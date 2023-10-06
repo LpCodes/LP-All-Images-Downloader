5002e94e9ad145118837c6cf68a71b7cf245f2da
LP <lovelesh_p@zohomail.in>
2023-10-06 13:35:41 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -1,31 +1,49 @@
 from setuptools import find_packages, setup
 
-
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# Project summary
+summary = "A Python package to automate image downloading from a given URL."
+
+# Keywords
+keywords = ['image', 'download', 'automation', 'web scraping', 'selenium', 'webdriver_manager']
+
+# License
+license = "MIT License"
+
+# Author email
+author_email = "lovelesh_p@zohomail.in"
+
+# Minimum Python version required
+python_requires = ">=3.6"
+
 setup(
     name="LpImagesDownloader",
-    version="0.0.8",
+    version="0.0.9",
     author="Lpcodes",
-    description="A package for downloading all the Images from the URL provided",
+    author_email=author_email,
+    description=summary,
     long_description_content_type="text/markdown",
     long_description=long_description,
     url="https://github.com/LpCodes/LP-All-Images-Downloader",
     project_urls={
         "Bug Tracker": "https://github.com/LpCodes/LP-All-Images-Downloader/issues",
+        "Source Code": "https://github.com/LpCodes/LP-All-Images-Downloader",
     },
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     packages=find_packages(),
-    keywords='Images Image download automation auto webscrapping web selenium',
+    keywords=keywords,
     install_requires=[
         'requests',
         'validators',
         'selenium',
         'webdriver_manager',
     ],
-    python_requires=">=3.6",)
+    python_requires=python_requires,
+)
